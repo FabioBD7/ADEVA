aadc6f7b3418bd25537e01479570d9f07276ad91
FabioBD7 <fabio.dias@usp.br>
2019-10-08 12:50:04 -0300

    criada a funcao pegaValor chamando as funcoes de operacao com if com o id do botao

--- calculadora/js/functions.js
+++ calculadora/js/functions.js
@@ -2,26 +2,33 @@ function injetaTexto(texto){
     var  saida = document.querySelector("#s");
     saida.textContent= texto;
 }
-function soma(n1id, n2id){
-    var n1 = document.querySelector(n1id);
-    var n2 = document.querySelector(n2id);
-    injetaTexto("a soma de "+n1.value+" com "+n2.value+" é igual a "+(parseFloat(n1.value)+ parseFloat(n2.value)))
+function soma(n1, n2){
+    injetaTexto("a soma de "+n1+" com "+n2+" é igual a "+(parseFloat(n1)+ parseFloat(n2)))
 }
-
-function subtrai(n1id, n2id){
-    var n1 = document.querySelector(n1id);
-    var n2 = document.querySelector(n2id);
-    injetaTexto("a subitração  de "+n1.value+" por "+n2.value+" é igual a "+(parseFloat(n1.value) - parseFloat(n2.value)));
+function subtrai(n1, n2){
+    injetaTexto("a subitração  de "+n1+" por "+n2+" é igual a "+(parseFloat(n1) - parseFloat(n2)));
     }
-
-function multiplica(n1id, n2id){
-    var n1 = document.querySelector(n1id);
-    var n2 = document.querySelector(n2id);
-    injetaTexto("a multiplicação   de "+n1.value+" por "+n2.value+" é igual a "+(parseFloat(n1.value) * parseFloat(n2.value)));
+function multiplica(n1, n2){
+    injetaTexto("a multiplicação   de "+n1+" por "+n2+" é igual a "+(parseFloat(n1) * parseFloat(n2)));
     }
-    function divide(n1id, n2id){
-        var n1 = document.querySelector(n1id);
-        var n2 = document.querySelector(n2id);
-        injetaTexto("a divisão    de "+n1.value+" por "+n2.value+" é igual a "+(parseFloat(n1.value) / parseFloat(n2.value)));
+    function divide(n1, n2){
+        injetaTexto("a divisão    de "+n1+" por "+n2+" é igual a "+(parseFloat(n1) / parseFloat(n2)));
         }
-    
+
+function pegaValor(n1id, n2id, bid){
+	n1 = document.querySelector(n1id);
+	n2 = document.querySelector(n2id);
+	
+	if(bid=="#btn-soma"){
+		soma(n1.value, n2.value);
+	}
+	if(bid=="#btn-subtrai"){
+		subtrai(n1.value, n2.value);
+	}
+	if(bid=="#btn-multiplica"){
+		multiplica(n1.value, n2.value);
+	}
+	if(bid=="#btn-divide"){
+		divide(n1.value, n2.value);
+	}
+}
